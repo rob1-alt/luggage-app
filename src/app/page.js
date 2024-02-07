7d27f555f5575ca0bd1a514f6cc8875f98c79caa
Robin <robin.pautigny@hetic.net>
2024-02-07 23:34:21 +0100

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import logo from "./lugaggeLogo.svg";
+import hero from "./bg.svg";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.navbar}>
+        <Image  
+          src={logo}
+          alt="logo"
+          width={160}
+          height={50}
+        />
+        <div className={styles.navbarLinks}>
+          <a href="#">Pricing</a>
+          <a href="#">FAQ</a>
+          <a href="#">About us</a>
+      </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.herobanner}>
+          <h1>Travel the world <br/>more easily</h1>
+          <p>Get your luggage delivered to your destination</p>
+          <Link href="https://ycfam1lw21x.typeform.com/to/jzWdzOzp" className={styles.button}>Get started</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
